4a3f7947d0830f5af02b26656f74c66f779636f0
Vitezslav Lindovsky <vitezslav.lindovsky@gmail.com>
2025-01-16 21:57:48 +0100

    Fix location of range end

--- internal/range.go
+++ internal/range.go
@@ -50,21 +50,39 @@ func (ri *RangeIndex) Add(hashStart string, hashEnd string) (existsStart bool, e
 	If hashStart key already exists, check the value didn't change (hashEnd)
 	If hashEnd does not exists, add it with empty value
 	*/
-	prefix := getPrefix(hashStart)
+	prefixStart := getPrefix(hashStart)
+	prefixEnd := getPrefix(hashEnd)
 
 	ri.mutex.RLock()
-	existingStartVal, existsStart := ri.Index[prefix][hashStart]
-	_, existsEnd = ri.Index[prefix][hashEnd]
+	existingStartValAsStart, existsStart := ri.Index[prefixStart][hashStart]
+	_, existsEnd = ri.Index[prefixEnd][hashEnd]
 	ri.mutex.RUnlock()
 
-	existsAndDifferentEnd := existsStart && existingStartVal != "" && existingStartVal != hashEnd
-	existsStartWithEmptyEnd := existsStart && existingStartVal == ""
+	// !existsStart = adding full chain from start to end
+	// !existsEnd adding end of chan as start with empty end
+
+	// existsAndDifferentEnd = start exists and end is different
+	existsAndDifferentEnd := existsStart && existingStartValAsStart != "" && existingStartValAsStart != hashEnd
+	// existsStartWithEmptyEnd = start exists and end is empty, from being End before
+	existsStartWithEmptyEnd := existsStart && existingStartValAsStart == ""
 	updateStartValue := !existsStart || existsStartWithEmptyEnd || (existsAndDifferentEnd && ri.ignoreChanges)
 
 	if existsAndDifferentEnd {
 		msg := "range starting %s already exists with different hashEnd! Existing: %s | New: %s"
-		err = fmt.Errorf(msg, hashStart, existingStartVal, hashEnd)
+		err = fmt.Errorf(msg, hashStart, existingStartValAsStart, hashEnd)
 	}
+
+	/*
+		debugging the brain-work here
+		if !existsStart && !existsEnd {
+			log.Printf("Adding range %s - %s\n", hashStart, hashEnd)
+		} else {
+			log.Printf("both exists %s - %s\n", hashStart, hashEnd)
+		}
+
+		msg := "existsStart %v | existsEnd %v | existsAndDifferentEnd %v | existsStartWithEmptyEnd %v | updateStartValue %v\n"
+		log.Printf(msg, existsStart, existsEnd, existsAndDifferentEnd, existsStartWithEmptyEnd, updateStartValue)
+	*/
 
 	if !existsStart {
 		ri.cntChains.Add(1)
@@ -76,13 +94,13 @@ func (ri *RangeIndex) Add(hashStart string, hashEnd string) (existsStart bool, e
 
 	if updateStartValue {
 		ri.mutex.Lock()
-		ri.Index[prefix][hashStart] = hashEnd
+		ri.Index[prefixStart][hashStart] = hashEnd
 		ri.mutex.Unlock()
 	}
 
 	if !existsEnd {
 		ri.mutex.Lock()
-		ri.Index[prefix][hashEnd] = ""
+		ri.Index[prefixEnd][hashEnd] = ""
 		ri.cntChainsEmpty.Add(1)
 		ri.mutex.Unlock()
 	}
@@ -90,6 +108,16 @@ func (ri *RangeIndex) Add(hashStart string, hashEnd string) (existsStart bool, e
 	return
 }
 
+// isHashInRange determines whether a given hash falls within any of the stored hash ranges.
+//
+// This function may produce false negatives in specific edge cases where the zone of hashes
+// is very small or spans across prefix boundaries. For example, consider a saved range with
+// a prefix starting at "10..." and ending at "30...", and we are looking for a hash "20...".
+// If the prefix derived from the hash is "20", the function might not find a match due to
+// how ranges are indexed by prefix.
+// However, as this function is only used to avoid querying domains whose hashes are already
+// within the known ranges, these false negatives are acceptable for its intended purpose.
+// If I get bored I will rewrite it to binary search.
 func (ri *RangeIndex) isHashInRange(hash string) (inRange bool, exactRange string) {
 	prefix := getPrefix(hash)
 
